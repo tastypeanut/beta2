cc4225fa94dfbbf59a39e32d6ceed61c426d1569
tastypeanut <tastypeanut@users.noreply.github.com>
2022-12-27 19:40:33 +0100

    test

--- .cloudflare/functions/contact/test.js
+++ .cloudflare/functions/contact/test.js
@@ -1,3 +1,15 @@
-export async function onRequest({ request, env }) {
-    return new Response('Variable: ' + env.EMAIL)
-}
+const mailchannelsPlugin = require('@cloudflare/pages-plugin-mailchannels');
+
+exports.onRequest({ request, env }) = mailchannelsPlugin({
+  personalizations: [
+    {
+      to: [{ name: "enrique.wtf - CONTACT FORM", email: "dtb9q2oshr@enrique.wtf" }],
+    },
+  ],
+  from: { name: "enrique.wtf - CONTACT FORM", email: "dtb9q2oshr@enrique.wtf" },
+  respondWith: () =>
+    new Response(null, {
+      status: 302,
+      headers: { Location: "/thank-you" },
+    }),
+});
