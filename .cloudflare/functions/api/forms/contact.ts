6b50edd596831edfb4fed4feb267c98c02aa27a0
tastypeanut <tastypeanut@users.noreply.github.com>
2022-12-27 03:51:28 +0100

    Ooopsie

---
+++ .cloudflare/functions/api/forms/contact.ts
@@ -0,0 +1,22 @@
+export function onRequest(context) {
+    return new Response(env.EMAIL)
+  }
+/*
+import mailChannelsPlugin from "@cloudflare/pages-plugin-mailchannels";
+
+export const onRequestPost(context): PagesFunction = mailChannelsPlugin({
+    personalizations: [{
+        to: [{
+            name: "ACME Support",
+            email: "ebenvenutto@outlook.com"
+        }],
+    }, ],
+    from: {
+        name: "ACME Support",
+        email: "support@example.com",
+    },
+    respondWith: () => {
+        return new Response(`Thank you for submitting your enquiry. A member of the team will be in touch shortly.`);
+    },
+});
+*/
